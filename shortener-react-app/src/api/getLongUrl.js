fde555e24d63ad29af6a957c456a1825f7847c7f
Franco-Micheletti <79060860+Franco-Micheletti@users.noreply.github.com>
2024-04-21 21:51:01 -0300

    fixed

--- shortener-react-app/src/api/getLongUrl.js
+++ shortener-react-app/src/api/getLongUrl.js
@@ -1,5 +1,5 @@
 export const getLongUrl = async (shortUrl) => {
-  const url = import.meta.env.PRODUCTION ? `${import.meta.env.BACKEND_URL}/url/get_url/short_url=${shortUrl}` : `http://127.0.0.1:8000/url/get_url/short_url=${shortUrl}`
+  const url = import.meta.env.VITE_PRODUCTION ? `${import.meta.env.VITE_BACKEND_URL}/url/get_url/short_url=${shortUrl}` : `http://127.0.0.1:8000/url/get_url/short_url=${shortUrl}`
   const response = await fetch(`${url}`, {
     method: 'GET',
     credentials: 'include',
