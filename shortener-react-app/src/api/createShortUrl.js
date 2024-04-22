57daf3ffed1395e01dbbbb3d33a1818a03b3b9e4
Franco-Micheletti <79060860+Franco-Micheletti@users.noreply.github.com>
2024-04-21 21:32:56 -0300

    response errors handling, throttling errors

--- shortener-react-app/src/api/createShortUrl.js
+++ shortener-react-app/src/api/createShortUrl.js
@@ -1,5 +1,5 @@
 export const createShortUrl = async (longUrl) => {
-  const url = import.meta.env.VITE_PRODUCTION ? `${import.meta.env.VITE_BACKEND_URL}/url/create_short_url` : 'http://127.0.0.1:8000/url/create_short_url'
+  const url = import.meta.env.VITE_PRODUCTION ? `https://${import.meta.env.VITE_BACKEND_URL}/url/create_short_url` : 'http://127.0.0.1:8000/url/create_short_url'
   const response = await fetch(`${url}`, {
     method: 'POST',
     credentials: 'include',
