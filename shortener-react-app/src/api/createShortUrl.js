4eaa32a6e7b0d2c4870fb9aff8008685d9682cae
Franco-Micheletti <79060860+Franco-Micheletti@users.noreply.github.com>
2024-04-21 21:15:41 -0300

    fix

--- shortener-react-app/src/api/createShortUrl.js
+++ shortener-react-app/src/api/createShortUrl.js
@@ -1,5 +1,6 @@
 export const createShortUrl = async (longUrl) => {
   console.log(import.meta.env.PRODUCTION)
+  console.log(process.env.PRODUCTION)
   const url = import.meta.env.PRODUCTION ? `${import.meta.env.BACKEND_URL}/url/create_short_url` : 'http://127.0.0.1:8000/url/create_short_url'
   const response = await fetch(`${url}`, {
     method: 'POST',
